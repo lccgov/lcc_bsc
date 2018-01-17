6e1a9d8f39a30825ea29b87736397be1be092476
Travis CI <developer@leeds.gov.uk>
2018-01-17 07:32:06 +0000

    design assests for bsc and bsc membership sites

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,6 @@
+//Application specific javascript to go here
+//= require google-analytics
+//= require handlebars
+//= require date-range-filter
+//= require news-archive-filter
+//= require keyword-filter
